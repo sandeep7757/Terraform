defa415a3af4f2c86d4f2670a21974ccde5909b3
sandeep <gopal7757@gmail.com>
2023-01-02 18:28:23 -0500

    adding files

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+terraform {
+  backend "s3" {
+    bucket = "pybuc"
+    key = "terraform"
+    region = "us-east-1"
+  }
+
+}
+resource "aws_sqs_queue" "sqs" {
+    name = "December2022"
+  
+}
