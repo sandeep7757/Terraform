34a2797d2e7da22bbd54d872f6a0202f7d3b8481
sandeep7757 <84711922+sandeep7757@users.noreply.github.com>
2023-01-02 19:29:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
     region = "us-east-1"
   
 }
-terraform {
-  backend "s3" {
-    bucket = "pybuc"
-    key = "terraform"
-    region = "us-east-1"
-  }
+#terraform {
+#  backend "s3" {
+ #   bucket = "pybuc"
+ #   key = "terraform"
+  #  region = "us-east-1"
+ # }
 
 }
 resource "aws_sqs_queue" "sqs" {
